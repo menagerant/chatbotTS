8163ef1de198fe489a6b077f7a0ec6dda777074e
Antoine MENAGER <antoinemenager@MacBook-Pro-de-Antoine.local>
2023-09-20 18:21:02 +0200

    ipAddress correcting 2

--- app/api/userlocation/route.js
+++ app/api/userlocation/route.js
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
+import { internalIpV6, internalIpV4 } from "internal-ip";
 
 export async function GET() {
   try {
-    const response = await fetch("https://api.ipify.org?format=json");
-    const data = await response.json();
-    const ipAddress = data.ip;
+    const ipAddress = await internalIpV4();
     const response2 = await fetch(
       `http://ip-api.com/json/${ipAddress}?lang=fr`
     );
