67d82d8a3b7b12299e2196474afe1dfb20300b65
Antoine MENAGER <antoinemenager@MacBook-Pro-de-Antoine.local>
2023-10-09 19:53:32 +0200

    test summary page 5

--- components/Dashboard/KPI.tsx
+++ components/Dashboard/KPI.tsx
@@ -9,7 +9,7 @@ export default function KPI() {
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-      <Tabs defaultValue="" className="w-full space-y-4">
+      <Tabs defaultValue="summary" className="w-full space-y-4">
         <TabsList>
           <TabsTrigger value="summary">Summary</TabsTrigger>
           <TabsTrigger value="tiktok">Chat Tiktok</TabsTrigger>
