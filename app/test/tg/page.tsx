e74d25ca87053055378ac21d55f75203bb9edb7d
Antoine MENAGER <antoinemenager@MacBook-Pro-de-Antoine.local>
2023-10-12 14:33:08 +0200

    test pages

--- app/test/tg/page.tsx
+++ app/test/tg/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Telegram/HeaderTG";
-import Chat from "@/components/Telegram/ChatTG";
+import Chat from "@/components/Test/TestChatTG";
 
-export default function TelegramManon() {
+export default function TestTelegram() {
   return (
     <div className="bg-[#F8F8F8] h-screen bg-[url('/backgroundtelegram.jpg')] bg-cover">
       <Header />
---
+++ app/test/tt/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Tiktok/HeaderTT";
+import Chat from "@/components/Test/TestChatTT";
+
+export default function TestTiktok() {
+  return (
+    <div className="bg-[#F8F8F8] h-screen">
+      <Header />
+      <Chat />
+    </div>
+  );
+}
